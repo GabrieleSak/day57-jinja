b2646f57197daede0fdebc5d46c49fc6a66c5c65
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2023-03-02 12:32:37 +0200

    Create template that dynamically insert name from the URL and age and gender from API genderize.io and agify.io

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import random
 import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -10,6 +11,14 @@ def home():
     random_number = random.randint(1, 10)
     current_year = datetime.date.today().year
     return render_template("index.html", num=random_number, year=current_year)
+
+@app.route("/guess/<name>")
+def guess(name):
+    age_response = requests.get(f"https://api.agify.io?name={name}")
+    age = age_response.json()["age"]
+    gender_response = requests.get(f"https://api.genderize.io?name={name}")
+    gender = gender_response.json()["gender"]
+    return render_template("name.html", name=name, gender=gender, age=age)
 
 
 if __name__ == "__main__":
